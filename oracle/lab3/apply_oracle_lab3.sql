8c562a8b04d18eb8c1e25692578b486e2732d76a
Tony Moraes <tonylds1@gmail.com>
2018-05-20 01:38:06 -0300

    lab 3 files

--- oracle/lab3/apply_oracle_lab3.sql
+++ oracle/lab3/apply_oracle_lab3.sql
@@ -14,8 +14,28 @@
 -- ----------------------------------------------------------------------
 
 -- Run the prior lab script.
+@/home/student/Data/cit225/oracle/lab2/create/apply_oracle_lab2.sql
+@/home/student/Data/cit225/oracle/lib2/preseed/preseed_oracle_store.sql
 
 
 -- Add your lab here:
+
+-- Open log file.
+SPOOL apply_oracle_lab3.txt
+-- Set the page size.
+SET ECHO ON
+SET PAGESIZE 999
+-- ----------------------------------------------------------------------
+-- Step #0 : CALL updated preseed script that has _lab corrections in it
+-- ----------------------------------------------------------------------
+SELECT 'Step #0 Call updated preseed script that has _lab corrections in it' 
+AS "Step Number" FROM dual;
+
+-- ----------------------------------------------------------------------
+-- Objective #1: Setup preseed for _LAB tables 
+-- ----------------------------------------------------------------------
+@preseed_oracle_store_lab_tables.sql
+spool off
+
 -- ----------------------------------------------------------------------
 
