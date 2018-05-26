f10dfdf6b6c24314c3e784c1022bdc303bbd2c89
Student <student@localhost.localdomain>
2018-05-26 04:02:55 -0700

    Fixing lab 3

--- oracle/lab4/apply_oracle_lab4.sql
+++ oracle/lab4/apply_oracle_lab4.sql
@@ -23,14 +23,14 @@
  
 -- insert calls to other SQL script files here, in the order that they appear in the seeding.sql script ...
 @@group_account1_lab.sql
-@@group_account2.sql
-@@group_account3.sql
-@@item_inserts.sql
-@@create_insert_contacts.sql
-@@individual_accounts.sql
-@@update_members.sql
-@@rental_inserts.sql
-@@create_view.sql
+@@group_account2_lab.sql
+@@group_account3_lab.sql
+@@item_inserts_lab.sql
+@@create_insert_contact_lab.sql
+@@individual_accounts_lab.sql
+@@update_member_labs.sql
+@@rental_inserts_lab.sql
+@@create_view_lab.sql
  
 SPOOL apply_oracle_lab4.txt
  
@@ -48,13 +48,13 @@ SPOOL apply_oracle_lab4.txt
 --    5. TELEPHONE
 -- ------------------------------------------------------------------
 
-COL member_id       FORMAT 9999 HEADING "Acct|ID #"
+COL member_lab_id       FORMAT 9999 HEADING "Acct|ID #"
 COL account_number  FORMAT A10  HEADING "Account|Number"
 COL full_name       FORMAT A16  HEADING "Name|(Last, First MI)"
 COL city            FORMAT A12  HEADING "City"
 COL state_province  FORMAT A10  HEADING "State"
-COL telephone       FORMAT A18  HEADING "Telephone"
-SELECT   m.member_id
+COL telephone_lab       FORMAT A18  HEADING "Telephone"
+SELECT   m.member_lab_id
 ,        m.account_number
 ,        c.last_name || ', ' || c.first_name
 ||       CASE
@@ -62,26 +62,26 @@ SELECT   m.member_id
          END AS full_name
 ,        a.city
 ,        a.state_province
-,        t.country_code || '-(' || t.area_code || ') ' || t.telephone_number AS telephone
-FROM     member m INNER JOIN contact c ON m.member_id = c.member_id INNER JOIN
-         address a ON c.contact_id = a.contact_id INNER JOIN
-         street_address sa ON a.address_id = sa.address_id INNER JOIN
-         telephone t ON c.contact_id = t.contact_id AND a.address_id = t.address_id
+,        t.country_code || '-(' || t.area_code || ') ' || t.telephone_lab_number AS telephone_lab
+FROM     member_lab m INNER JOIN contact_lab c ON m.member_lab_id = c.member_lab_id INNER JOIN
+         address_lab a ON c.contact_lab_id = a.contact_lab_id INNER JOIN
+         street_address_lab sa ON a.address_lab_id = sa.address_lab_id INNER JOIN
+         telephone_lab t ON c.contact_lab_id = t.contact_lab_id AND a.address_lab_id = t.address_lab_id
 WHERE    last_name IN ('Sweeney','Vizquel','Winn');
 
 -- ------------------------------------------------------------------
---  Display the 21 inserts into the item table.
+--  Display the 21 inserts into the item_lab table.
 -- ------------------------------------------------------------------
 SET PAGESIZE 99
-COL item_id                FORMAT 9999  HEADING "Item|ID #"
-COL common_lookup_meaning  FORMAT A20  HEADING "Item Description"
-COL item_title             FORMAT A30  HEADING "Item Title"
-COL item_release_date      FORMAT A11  HEADING "Item|Release|Date"
-SELECT   i.item_id
-,        cl.common_lookup_meaning
-,        i.item_title
-,        i.item_release_date
-FROM     item i INNER JOIN common_lookup cl ON i.item_type = cl.common_lookup_id;
+COL item_lab_id                FORMAT 9999  HEADING "Item|ID #"
+COL common_lookup_lab_meaning  FORMAT A20  HEADING "Item Description"
+COL item_lab_title             FORMAT A30  HEADING "Item Title"
+COL item_lab_release_date      FORMAT A11  HEADING "Item|Release|Date"
+SELECT   i.item_lab_id
+,        cl.common_lookup_lab_meaning
+,        i.item_lab_title
+,        i.item_lab_release_date
+FROM     item_lab i INNER JOIN common_lookup_lab cl ON i.item_lab_type = cl.common_lookup_lab_id;
 
 -- ------------------------------------------------------------------
 --   Query to verify five individual rows of chained inserts through
@@ -91,7 +91,7 @@ COL account_number  FORMAT A10  HEADING "Account|Number"
 COL full_name       FORMAT A20  HEADING "Name|(Last, First MI)"
 COL city            FORMAT A12  HEADING "City"
 COL state_province  FORMAT A10  HEADING "State"
-COL telephone       FORMAT A18  HEADING "Telephone"
+COL telephone_lab       FORMAT A18  HEADING "Telephone"
 SELECT   m.account_number
 ,        c.last_name || ', ' || c.first_name
 ||       CASE
@@ -99,39 +99,39 @@ SELECT   m.account_number
          END AS full_name
 ,        a.city
 ,        a.state_province
-,        t.country_code || '-(' || t.area_code || ') ' || t.telephone_number AS telephone
-FROM     member m INNER JOIN contact c ON m.member_id = c.member_id INNER JOIN
-         address a ON c.contact_id = a.contact_id INNER JOIN
-         street_address sa ON a.address_id = sa.address_id INNER JOIN
-         telephone t ON c.contact_id = t.contact_id AND a.address_id = t.address_id
-WHERE    m.member_type = (SELECT common_lookup_id
-                          FROM   common_lookup
-                          WHERE  common_lookup_context = 'MEMBER'
-                          AND    common_lookup_type = 'INDIVIDUAL');
+,        t.country_code || '-(' || t.area_code || ') ' || t.telephone_lab_number AS telephone_lab
+FROM     member_lab m INNER JOIN contact_lab c ON m.member_lab_id = c.member_lab_id INNER JOIN
+         address_lab a ON c.contact_lab_id = a.contact_lab_id INNER JOIN
+         street_address_lab sa ON a.address_lab_id = sa.address_lab_id INNER JOIN
+         telephone_lab t ON c.contact_lab_id = t.contact_lab_id AND a.address_lab_id = t.address_lab_id
+WHERE    m.member_lab_type = (SELECT common_lookup_lab_id
+                          FROM   common_lookup_lab
+                          WHERE  common_lookup_lab_context = 'MEMBER'
+                          AND    common_lookup_lab_type = 'INDIVIDUAL');
 
 -- ------------------------------------------------------------------
 --   Query to verify nine rental agreements, some with one and some
---   with more than one rental item.
+--   with more than one rental item_lab.
 -- ------------------------------------------------------------------
-COL member_id       FORMAT 9999 HEADING "Member|ID #"
+COL member_lab_id       FORMAT 9999 HEADING "Member|ID #"
 COL account_number  FORMAT A10  HEADING "Account|Number"
 COL full_name       FORMAT A20  HEADING "Name|(Last, First MI)"
 COL rental_id       FORMAT 9999 HEADING "Rent|ID #"
-COL rental_item_id  FORMAT 9999 HEADING "Rent|Item|ID #"
-COL item_title      FORMAT A26  HEADING "Item Title"
-SELECT   m.member_id
+COL rental_item_lab_id  FORMAT 9999 HEADING "Rent|Item|ID #"
+COL item_lab_title      FORMAT A26  HEADING "Item Title"
+SELECT   m.member_lab_id
 ,        m.account_number
 ,        c.last_name || ', ' || c.first_name
 ||       CASE
            WHEN c.middle_name IS NOT NULL THEN ' ' || SUBSTR(c.middle_name,1,1)
          END AS full_name
 ,        r.rental_id
-,        ri.rental_item_id
-,        i.item_title
-FROM     member m INNER JOIN contact c ON m.member_id = c.member_id INNER JOIN
-         rental r ON c.contact_id = r.customer_id INNER JOIN
-         rental_item ri ON r.rental_id = ri.rental_id INNER JOIN
-         item i ON ri.item_id = i.item_id
+,        ri.rental_item_lab_id
+,        i.item_lab_title
+FROM     member_lab m INNER JOIN contact_lab c ON m.member_lab_id = c.member_lab_id INNER JOIN
+         rental r ON c.contact_lab_id = r.customer_id INNER JOIN
+         rental_item_lab ri ON r.rental_id = ri.rental_id INNER JOIN
+         item_lab i ON ri.item_lab_id = i.item_lab_id
 ORDER BY r.rental_id;
  
 SPOOL OFF
---
+++ oracle/lab4/testSql.sql
@@ -0,0 +1,4 @@
+SELECT	*
+FROM	common_lookup_lab;
+--WHERE	common_lookup_lab_context = 'member_lab'-
+--AND	common_lookup_lab_type = 'DISCOVER_CARD';
