d0faddd5c7e9a59858ccf6c0fe5c5c2aed2f1a1f
Tony Moraes <tonylds1@gmail.com>
2018-07-18 14:59:51 -0300

    lab 09

--- oracle/lab9/apply_oracle_lab9.sql
+++ oracle/lab9/apply_oracle_lab9.sql
@@ -15,3 +15,324 @@
 
 -- Run the prior lab script.
 @/home/student/Data/cit225/oracle/lab8/apply_oracle_lab8.sql
+
+SPOOL apply_oracle_lab9.txt
+ 
+-- Step#1 - [4 points] Create the following TRANSACTION table as per the specification, but do so understanding the business logic of the model. After creating the TRANSACTION table, create a unique index on the columns that make up the natural key and call it the NATURAL_KEY index.
+
+--Step #1a - You create the TRANSACTION table and a TRANSACTION_S1 sequence as qualified below.
+
+-- Verification Step #1a
+COLUMN table_name   FORMAT A14  HEADING "Table Name"
+COLUMN column_id    FORMAT 9999 HEADING "Column ID"
+COLUMN column_name  FORMAT A22  HEADING "Column Name"
+COLUMN nullable     FORMAT A8   HEADING "Nullable"
+COLUMN data_type    FORMAT A12  HEADING "Data Type"
+SELECT   table_name
+,        column_id
+,        column_name
+,        CASE
+           WHEN nullable = 'N' THEN 'NOT NULL'
+           ELSE ''
+         END AS nullable
+,        CASE
+           WHEN data_type IN ('CHAR','VARCHAR2','NUMBER') THEN
+             data_type||'('||data_length||')'
+           ELSE
+             data_type
+         END AS data_type
+FROM     user_tab_columns
+WHERE    table_name = 'TRANSACTION'
+ORDER BY 2;
+
+-- Step #1b - You create the NATURAL_KEY unique index as qualified below.
+
+-- Verification Step #1b
+COLUMN table_name       FORMAT A12  HEADING "Table Name"
+COLUMN index_name       FORMAT A16  HEADING "Index Name"
+COLUMN uniqueness       FORMAT A8   HEADING "Unique"
+COLUMN column_position  FORMAT 9999 HEADING "Column Position"
+COLUMN column_name      FORMAT A24  HEADING "Column Name"
+SELECT   i.table_name
+,        i.index_name
+,        i.uniqueness
+,        ic.column_position
+,        ic.column_name
+FROM     user_indexes i INNER JOIN user_ind_columns ic
+ON       i.index_name = ic.index_name
+WHERE    i.table_name = 'TRANSACTION'
+AND      i.uniqueness = 'UNIQUE'
+AND      i.index_name = 'NATURAL_KEY';
+
+-- Step #2 - [2 points] Insert the following two TRANSACTION_TYPE rows and four PAYMENT_METHOD_TYPE rows into the COMMON_LOOKUP table. They should have valid who-audit column data.
+
+-- Verification Step #2
+COLUMN common_lookup_table  FORMAT A20 HEADING "COMMON_LOOKUP_TABLE"
+COLUMN common_lookup_column FORMAT A20 HEADING "COMMON_LOOKUP_COLUMN"
+COLUMN common_lookup_type   FORMAT A20 HEADING "COMMON_LOOKUP_TYPE"
+SELECT   common_lookup_table
+,        common_lookup_column
+,        common_lookup_type
+FROM     common_lookup
+WHERE    common_lookup_table = 'TRANSACTION'
+AND      common_lookup_column IN ('TRANSACTION_TYPE','PAYMENT_METHOD_TYPE')
+ORDER BY 1, 2, 3 DESC;
+
+-- Step 3 - [14 points] Create the following AIRPORT and ACCOUNT_LIST tables as per the specification, but do so understanding the business logic of the model.
+
+-- Step #3a - You need to create the AIRPORT table and the AIRPORT_S1 sequences.
+
+-- Verification Step #3a
+COLUMN table_name   FORMAT A14  HEADING "Table Name"
+COLUMN column_id    FORMAT 9999 HEADING "Column ID"
+COLUMN column_name  FORMAT A22  HEADING "Column Name"
+COLUMN nullable     FORMAT A8   HEADING "Nullable"
+COLUMN data_type    FORMAT A12  HEADING "Data Type"
+SELECT   table_name
+,        column_id
+,        column_name
+,        CASE
+           WHEN nullable = 'N' THEN 'NOT NULL'
+           ELSE ''
+         END AS nullable
+,        CASE
+           WHEN data_type IN ('CHAR','VARCHAR2','NUMBER') THEN
+             data_type||'('||data_length||')'
+           ELSE
+             data_type
+         END AS data_type
+FROM     user_tab_columns
+WHERE    table_name = 'AIRPORT'
+ORDER BY 2;
+
+-- Step #3b - You need to create a unique natural key (named NK_AIRPORT) index for the AIRPORT table. You should create it with the following four columns.
+
+-- Verification Step #3b
+COLUMN table_name       FORMAT A12  HEADING "Table Name"
+COLUMN index_name       FORMAT A16  HEADING "Index Name"
+COLUMN uniqueness       FORMAT A8   HEADING "Unique"
+COLUMN column_position  FORMAT 9999 HEADING "Column Position"
+COLUMN column_name      FORMAT A24  HEADING "Column Name"
+SELECT   i.table_name
+,        i.index_name
+,        i.uniqueness
+,        ic.column_position
+,        ic.column_name
+FROM     user_indexes i INNER JOIN user_ind_columns ic
+ON       i.index_name = ic.index_name
+WHERE    i.table_name = 'AIRPORT'
+AND      i.uniqueness = 'UNIQUE'
+AND      i.index_name = 'NK_AIRPORT';
+
+-- Step #3c - You need to seed the AIRPORT table with at least these cities, and any others that you’ve used for inserted values in the CONTACT table.
+
+-- Verification Step #3c
+COLUMN code           FORMAT A4  HEADING "Code"
+COLUMN airport_city   FORMAT A14 HEADING "Airport City"
+COLUMN city           FORMAT A14 HEADING "City"
+COLUMN state_province FORMAT A10 HEADING "State or|Province"
+SELECT   airport_code AS code
+,        airport_city
+,        city
+,        state_province
+FROM     airport;
+
+-- Step #3d - You need to create the ACCOUNT_LIST table and ACCOUNT_LIST_S1 sequence.
+
+-- Verification Step #3d
+COLUMN table_name   FORMAT A14
+COLUMN column_id    FORMAT 9999
+COLUMN column_name  FORMAT A22
+COLUMN data_type    FORMAT A12
+SELECT   table_name
+,        column_id
+,        column_name
+,        CASE
+           WHEN nullable = 'N' THEN 'NOT NULL'
+           ELSE ''
+         END AS nullable
+,        CASE
+           WHEN data_type IN ('CHAR','VARCHAR2','NUMBER') THEN
+             data_type||'('||data_length||')'
+           ELSE
+             data_type
+         END AS data_type
+FROM     user_tab_columns
+WHERE    table_name = 'ACCOUNT_LIST'
+ORDER BY 2;
+
+-- Step #3e - You need to seed the ACCOUNT_LIST table. This would take more time than leveraging a stored procedure but writing stored procedures from class is a beyond scope. Therefore, you’ve been provided with stored procedures for the databases. Click on the Setup Script to see how the procedure works, how to run the procedure, and how to check how many rows are inserted into the ACCOUNT_LIST table.
+
+-- Create or replace seeding procedure.
+CREATE OR REPLACE PROCEDURE seed_account_list IS
+  /* Declare variable to capture table, and column. */
+  lv_table_name   VARCHAR2(90);
+  lv_column_name  VARCHAR2(30);
+ 
+  /* Declare an exception variable and PRAGMA map. */
+  not_null_column  EXCEPTION;
+  PRAGMA EXCEPTION_INIT(not_null_column,-1400);
+ 
+BEGIN
+  /* Set savepoint. */
+  SAVEPOINT all_or_none;
+ 
+  FOR i IN (SELECT DISTINCT airport_code FROM airport) LOOP
+    FOR j IN 1..50 LOOP
+ 
+      INSERT INTO account_list
+      VALUES
+      ( account_list_s1.NEXTVAL
+      , i.airport_code||'-'||LPAD(j,6,'0')
+      , NULL
+      , NULL
+      , 1002
+      , SYSDATE
+      , 1002
+      , SYSDATE);
+    END LOOP;
+  END LOOP;
+ 
+  /* Commit the writes as a group. */
+  COMMIT;
+ 
+EXCEPTION
+  WHEN not_null_column THEN
+    /* Capture the table and column name that triggered the error. */
+    lv_table_name := (TRIM(BOTH '"' FROM RTRIM(REGEXP_SUBSTR(SQLERRM,'".*\."',REGEXP_INSTR(SQLERRM,'\.',1,1)),'."')));
+    lv_column_name := (TRIM(BOTH '"' FROM REGEXP_SUBSTR(SQLERRM,'".*"',REGEXP_INSTR(SQLERRM,'\.',1,2))));
+ 
+    /* This undoes all DML statements to this point in the procedure. */
+    ROLLBACK TO SAVEPOINT all_or_none;
+    RAISE_APPLICATION_ERROR(
+       -20001
+      ,'Remove the NOT NULL contraint from the '||lv_column_name||' column in'||CHR(10)||' the '||lv_table_name||' table.');
+  WHEN OTHERS THEN
+    /* This undoes all DML statements to this point in the procedure. */
+    ROLLBACK TO SAVEPOINT all_or_none;
+END;
+
+EXECUTE seed_account_list();
+
+-- Verification Step #3e
+COLUMN airport FORMAT A7
+SELECT   SUBSTR(account_number,1,3) AS "Airport"
+,        COUNT(*) AS "# Accounts"
+FROM     account_list
+WHERE    consumed_date IS NULL
+GROUP BY SUBSTR(account_number,1,3)
+ORDER BY 1;
+
+COLUMN airport FORMAT A7
+SELECT   SUBSTR(account_number,1,3) AS "Airport"
+,        COUNT(*) AS "# Accounts"
+FROM     account_list
+WHERE    consumed_date IS NULL
+GROUP BY SUBSTR(account_number,1,3)
+ORDER BY 1;
+
+-- Step #3f - In a prior lab and in both the create and seed scripts, the STATE_PROVINCE column values uses a mixture of US Postal Services state abbreviations and full state names. You need to update all STATE_PROVINCE values with their full state names because a subsequent seeding and the import program rely on full STATE_PROVINCE names. You need to update any pre-seeded US Postal Service state abbreviations with the full state names.
+UPDATE address
+SET    state_province = 'California'
+WHERE  state_province = 'CA';
+
+-- Step #3g - You need to run the script that creates the UPDATE_MEMBER_ACCOUNT procedure, and then you need to call it to update values in the MEMBER and ACCOUNT_LIST tables. It relies on you having run the SEED_ACCOUNT_LIST procedure successfully.
+CREATE OR REPLACE PROCEDURE update_member_account IS
+ 
+  /* Declare a local variable. */
+  lv_account_number VARCHAR2(10);
+ 
+  /* Declare a SQL cursor fabricated from local variables. */  
+  CURSOR member_cursor IS
+    SELECT   DISTINCT
+             m.member_id
+    ,        a.city
+    ,        a.state_province
+    FROM     member m INNER JOIN contact c
+    ON       m.member_id = c.member_id INNER JOIN address a
+    ON       c.contact_id = a.contact_id
+    ORDER BY m.member_id;
+ 
+BEGIN
+ 
+  /* Set savepoint. */  
+  SAVEPOINT all_or_none;
+ 
+  /* Open a local cursor. */  
+  FOR i IN member_cursor LOOP
+ 
+      /* Secure a unique account number as they're consumed from the list. */
+      SELECT al.account_number
+      INTO   lv_account_number
+      FROM   account_list al INNER JOIN airport ap
+      ON     SUBSTR(al.account_number,1,3) = ap.airport_code
+      WHERE  ap.city = i.city
+      AND    ap.state_province = i.state_province
+      AND    consumed_by IS NULL
+      AND    consumed_date IS NULL
+      AND    ROWNUM < 2;
+ 
+      /* Update a member with a unique account number linked to their nearest airport. */
+      UPDATE member
+      SET    account_number = lv_account_number
+      WHERE  member_id = i.member_id;
+ 
+      /* Mark consumed the last used account number. */      
+      UPDATE account_list
+      SET    consumed_by = 1002
+      ,      consumed_date = SYSDATE
+      WHERE  account_number = lv_account_number;
+ 
+  END LOOP;
+ 
+  /* Commit the writes as a group. */
+  COMMIT;
+ 
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    dbms_output.put_line('You have an error in your AIRPORT table inserts.');
+ 
+    /* This undoes all DML statements to this point in the procedure. */
+    ROLLBACK TO SAVEPOINT all_or_none;
+  WHEN OTHERS THEN
+    /* This undoes all DML statements to this point in the procedure. */
+    ROLLBACK TO SAVEPOINT all_or_none;
+END;
+
+EXECUTE update_member_account();
+
+-- Verification Step #3g
+COLUMN object_name FORMAT A22
+COLUMN object_type FORMAT A12
+SELECT   object_name
+,        object_type
+FROM     user_objects
+WHERE    object_name = 'UPDATE_MEMBER_ACCOUNT';
+
+-- Format the SQL statement display.
+COLUMN member_id      FORMAT 999999 HEADING "Member|ID #"
+COLUMN last_name      FORMAT A7     HEADING "Last|Name"
+COLUMN account_number FORMAT A10    HEADING "Account|Number"
+COLUMN acity          FORMAT A12    HEADING "Address City"
+COLUMN apstate        FORMAT A10    HEADING "Airport|State or|Province"
+COLUMN alcode         FORMAT A5     HEADING "Airport|Account|Code"
+ 
+-- Query distinct members and addresses.
+SELECT   DISTINCT
+         m.member_id
+,        c.last_name
+,        m.account_number
+,        a.city AS acity
+,        ap.state_province AS apstate
+,        SUBSTR(al.account_number,1,3) AS alcode
+FROM     member m INNER JOIN contact c
+ON       m.member_id = c.member_id INNER JOIN address a
+ON       c.contact_id = a.contact_id INNER JOIN airport ap
+ON       a.city = ap.city
+AND      a.state_province = ap.state_province INNER JOIN account_list al
+ON       ap.airport_code = SUBSTR(al.account_number,1,3)
+ORDER BY 1;
+
+-- Step #4 - [5 points] Create the following TRANSACTION_UPLOAD table as per the specification, but do so understanding the business logic of the model. As a BIG PICTURE, our video store just got a consolidation of rentals from a store that’s closed for our customers. We need to import the values and ensure that our source data agrees with the other store. For example, do all customer names, addresses, account numbers match our data. If they do, the merge should go well. If they don’t, the figures will ultimately be incorrect.
+
+SPOOL OFF
