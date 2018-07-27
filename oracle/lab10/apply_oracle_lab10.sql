db9833ec5d59b81225ec4436438f9f657f559a1a
Student <student@localhost.localdomain>
2018-07-27 12:16:27 -0700

    Lab 10 finished

--- oracle/lab10/apply_oracle_lab10.sql
+++ oracle/lab10/apply_oracle_lab10.sql
@@ -106,9 +106,9 @@ FROM (SELECT DISTINCT r.rental_lab_id
         ,      c.contact_lab_id
         ,      TRUNC(tu.check_out_date) AS check_out_date
         ,      TRUNC(tu.return_date) AS return_date
-        ,      3 AS created_by
+        ,      1 AS created_by
         ,      TRUNC(SYSDATE) AS creation_date
-        ,      3 AS last_updated_by
+        ,      1 AS last_updated_by
         ,      TRUNC(SYSDATE) AS last_update_date
         FROM member_lab m 
         INNER JOIN contact_lab c ON m.member_lab_id = c.member_lab_id 
@@ -130,9 +130,9 @@ SELECT   r.rental_lab_id
          ,        tu.item_id
          ,        TRUNC(r.return_date) - TRUNC(r.check_out_date) AS rental_item_lab_price
          ,        cl.common_lookup_lab_id AS rental_item_lab_type
-         ,        3 AS created_by
+         ,        1 AS created_by
          ,        TRUNC(SYSDATE) AS creation_date
-         ,        3 AS last_updated_by
+         ,        1 AS last_updated_by
          ,        TRUNC(SYSDATE) AS last_update_date
          FROM member_lab m INNER JOIN contact_lab c
       ON   m.member_lab_id = c.member_lab_id INNER JOIN transaction_upload tu
@@ -169,9 +169,9 @@ FROM (
          ,        tu.item_id
          ,        TRUNC(r.return_date) - TRUNC(r.check_out_date) AS rental_item_lab_price
          ,        cl.common_lookup_lab_id AS rental_item_lab_type
-         ,        3 AS created_by
+         ,        1 AS created_by
          ,        TRUNC(SYSDATE) AS creation_date
-         ,        3 AS last_updated_by
+         ,        1 AS last_updated_by
          ,        TRUNC(SYSDATE) AS last_update_date
          FROM member_lab m INNER JOIN contact_lab c
          ON   m.member_lab_id = c.member_lab_id INNER JOIN transaction_upload tu
@@ -191,7 +191,33 @@ FROM     rental_item_lab;
 
 -- Step #3 - [15 points] The third SELECT statement requires that you inserted values into the RENTAL_ITEM table in the last step. It also leverages the joins that you worked out in the first and secondSELECT statements. Don’t try to re-invent the wheel because it isn’t profitable, especially in this case.
 
-
+INSERT INTO rental_lab
+SELECT NVL(r.rental_lab_id,rental_lab_s1.nextval) AS rental_lab_id
+,      r.contact_lab_id
+,      r.check_out_date
+,      r.return_date
+,      r.created_by
+,      r.creation_date
+,      r.last_updated_by
+,      r.last_update_date
+FROM (SELECT DISTINCT r.rental_lab_id as rental_lab_id
+        ,      c.contact_lab_id as contact_lab_id
+        ,      TRUNC(tu.check_out_date) AS check_out_date
+        ,      TRUNC(tu.return_date) AS return_date
+        ,      1 AS created_by
+        ,      TRUNC(SYSDATE) AS creation_date
+        ,      1 AS last_updated_by
+        ,      TRUNC(SYSDATE) AS last_update_date
+        FROM member_lab m 
+        INNER JOIN contact_lab c ON m.member_lab_id = c.member_lab_id 
+        INNER JOIN transaction_upload tu ON c.first_name = tu.first_name
+        AND  NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+        AND  c.last_name = tu.last_name
+        AND  tu.account_number = m.account_number 
+        LEFT JOIN rental_lab r ON c.contact_lab_id = r.customer_id
+        AND  TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+        AND  TRUNC(tu.return_date) = TRUNC(r.return_date)
+) r;
 
 
 SPOOL OFF
